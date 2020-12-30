2a01456f27ac199b04b33209efaf10da78b27f7c
kixelated <kixelated@gmail.com>
2020-12-29 19:50:19 -0800

    Quick rename.

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,24 @@ use std::os::unix::io::AsRawFd;
 use wisp::{Runtime, Task};
 
 fn main() -> anyhow::Result<()> {
-    let mut runtime = Runtime::new(256)?;
+    let mut io = Runtime::new(256)?;
 
     let listener = TcpListener::bind(("127.0.0.1", 8080))?;
     let listener_fd = listener.as_raw_fd();
     println!("listen {}", listener.local_addr()?);
 
-    runtime.run(Task::Accept{fd: listener_fd})?;
+    io.run(Task::Accept{fd: listener_fd})?;
 
     loop {
-        let (_id, task, ret) = runtime.wait()?;
+        let (_id, task, ret) = io.wait()?;
 
         match task {
             Task::Accept{..} => {
                 let fd = ret?;
                 let buffer = vec![0u8; 2048].into_boxed_slice();
 
-                runtime.run(Task::Accept{fd: listener_fd})?;
-                runtime.run(Task::Read{fd: fd, buffer: buffer})?;
+                io.run(Task::Accept{fd: listener_fd})?;
+                io.run(Task::Read{fd: fd, buffer: buffer})?;
             },
             Task::Close{..} => {
                 println!("closed");
@@ -35,16 +35,17 @@ fn main() -> anyhow::Result<()> {
                 let size = ret? as usize;
 
                 if size == 0 {
-                    println!("EOF");
-
+                    println!("unexpected EOF");
+                    io.run(Task::Close{fd: fd})?;
+                    continue
                 }
 
                 let buffer = &buffer[..size];
 
                 println!("read {}", String::from_utf8_lossy(buffer));
 
-                let _ = runtime.run(Task::Write{fd: fd, buffer: buffer.into()})?;
-                let _ = runtime.run_after(Task::Close{fd: fd})?;
+                let _ = io.run(Task::Write{fd: fd, buffer: buffer.into()})?;
+                let _ = io.run_after(Task::Close{fd: fd})?;
             },
             Task::Write{ buffer, .. } => {
                 let size = ret? as usize;
